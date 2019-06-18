4fe32481b6ea252e7fdb5aeb266a5d3f8aa7860d
Ayumu Saito <mu_112ayu@yahoo.ne.jp>
2019-06-18 18:33:00 +0900

    done

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,30 @@ int n;
 int k;
 int A[100000];
 
+int p(int x){
+    int l, j, y;
+    l = 0;
+    for(j=0; j < n; j++){
+        l = ((A[j] + x - 1) / x) + l;
+    }
+    if(l <= k) y = 1;
+    else y = 0;
+    return y;
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+    lb = 0;
+    ub = 10000;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    while(ub - lb > 1){
+        int m = (ub + lb) / 2;
+        if(p(m)) ub = m;
+        else lb = m;
+    }
+  printf("%d\n",ub);
   return 0;
 }
