dfda386308e30835612f54438f27b4623b1a95f2
Ayumu Saito <mu_112ayu@yahoo.ne.jp>
2019-06-20 16:53:48 +0900

    done

--- src/spear.c
+++ src/spear.c
@@ -5,13 +5,29 @@ int k;
 int A[100000];
 
 
+int cannotMake(int x){
+    int l, i, y;
+    l = 0;
+    for(i=0; i < n; i++){
+        l = (A[i] / x) + l;
+    }
+    if(l < k) return (1);
+    else return (0);
+}
+
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+    int i, lb, ub;
+    scanf("%d%d", &n, &k);
+    lb = 0;
+    ub = 100000;
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+    }
+    while(ub - lb > 1){
+        int m = (ub + lb) / 2;
+        if(cannotMake(m)) ub = m;
+        else lb = m;
+    }
+    printf("%d\n",lb);
+    return 0;
 }
