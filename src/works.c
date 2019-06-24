dfa11cf06452e7415070102d35707a876b379660
Ayumu Saito <mu_112ayu@yahoo.ne.jp>
2019-06-24 19:03:52 +0900

    done

--- src/works.c
+++ src/works.c
@@ -5,42 +5,50 @@ int k;
 int A[100000];
 int B[100000];
 
-int between(int x, int y, int A[]){
-    int i, l;
-    for(i=x; i < y; i++){
-        l = A[i] + l;
-    }
-    return l;
+int abs(int x){
+    if(x < 0) return (-x);
+    else return (x);
 }
 
 int canCut(int x, int A[]){
-    int l, i, r;
+    int l, i, s;
     l = 0;
-    r = 0;
+    s = 0;
     for(i=0; i < x; i++){
         l = A[i] + l;
     }
-    for(i=x; i < n; i++){
-        r = A[i] + r;
+    for(i=0; i < n; i++){
+        s = A[i] + s;
     }
-    if(l > (r / (k-1))) return (1);
+    if(l * k > s) return (1);
     else return (0);
 }
 
 
 int main(){
-    int i, lb, ub, a, b;
+    int i, lb, ub, a, b, sum, u;
     scanf("%d%d", &n, &k);
-    lb = 0;
-    ub = n;
     for(i = 0; i < n; i++){
         scanf("%d", &A[i]);
     }
+    lb = 0;
+    ub = 0;
+    u = n;
     while(k >= 2){
-        b = 0;
-        for(i=lb; i < n-lb; i++){
-            B[i-lb] = A[i];
+        for(i=0; i < n; i++){
+            B[i] = 0;
         }
+        sum = 0;
+        b = 0;
+        u = u - lb;
+        for(i=n-u; i < n; i++){
+            B[i-n+u] = A[i];
+        }
+        for(i=0; i < n-lb; i++){
+            sum = B[i] + sum;
+        }
+        lb = 0;
+        ub = u - k + 1;
         while(ub - lb > 1){
             int m = (ub + lb) / 2;
             if(canCut(m,B)) ub = m;
@@ -49,16 +57,26 @@ int main(){
         for(i=0; i < lb; i++){
             b = B[i] + b;
         }
-        if(a > b) a = a;
-        else a = b;
+        if(lb == 0){
+            b = B[0];
+            lb = lb + 1;
+        }
+        else{
+            if(abs(sum - b * k) > abs(sum - (b + B[lb]) * k)){
+            b = b + B[lb];
+            lb = lb + 1;
+        }
+        }
+        if(a < b) a = b;
         k = k - 1;
+        for(i=0; i<n; i++){
+        }
     }
     b = 0;
     for(i=lb; i < n; i++){
         b = B[i] + b;
     }
-    if(a > b) a = a;
-    else a = b;
+    if(a < b) a = b;
     printf("%d\n",a);
     return 0;
 }
