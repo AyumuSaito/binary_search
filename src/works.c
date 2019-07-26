6cf1f4e44540a7687bf84d67410a4835be98bed6
AyumuSaito <51943501+AyumuSaito@users.noreply.github.com>
2019-07-26 15:40:40 +0900

    Update works.c

--- src/works.c
+++ src/works.c
@@ -3,80 +3,35 @@
 int n;
 int k;
 int A[100000];
-int B[100000];
 
-int abs(int x){
-    if(x < 0) return (-x);
-    else return (x);
-}
-
-int canCut(int x, int A[]){
-    int l, i, s;
-    l = 0;
-    s = 0;
-    for(i=0; i < x; i++){
-        l = A[i] + l;
-    }
+int p(int x){
+    int mem, i, sum;
+    mem = 1;    //一人の仕事量をx以下としたときの必要な人数をカウント
+    sum = 0;
     for(i=0; i < n; i++){
-        s = A[i] + s;
+        if(A[i] > x) return 0;
+        if(sum + A[i] <= x) sum += A[i];
+        else {
+            mem++;
+            sum = A[i];
+        }
     }
-    if(l * k > s) return (1);
-    else return (0);
+    return mem <= k;    //必要な人数が与えられたkより小さいか否か
 }
-
 
 int main(){
-    int i, lb, ub, a, b, sum, u;
+    int i, lb, ub;
     scanf("%d%d", &n, &k);
+    lb = 0;
+    ub = 1000000000;
     for(i = 0; i < n; i++){
         scanf("%d", &A[i]);
     }
-    lb = 0;
-    ub = 0;
-    u = n;
-    while(k >= 2){
-        for(i=0; i < n; i++){
-            B[i] = 0;
-        }
-        sum = 0;
-        b = 0;
-        u = u - lb;
-        for(i=n-u; i < n; i++){
-            B[i-n+u] = A[i];
-        }
-        for(i=0; i < n-lb; i++){
-            sum = B[i] + sum;
-        }
-        lb = 0;
-        ub = u - k + 1;
-        while(ub - lb > 1){
-            int m = (ub + lb) / 2;
-            if(canCut(m,B)) ub = m;
-            else lb = m;
-        }
-        for(i=0; i < lb; i++){
-            b = B[i] + b;
-        }
-        if(lb == 0){
-            b = B[0];
-            lb = lb + 1;
-        }
-        else{
-            if(abs(sum - b * k) > abs(sum - (b + B[lb]) * k)){
-                b = b + B[lb];
-                lb = lb + 1;
-            }
-        }
-        if(a < b) a = b;
-        k = k - 1;
-        for(i=0; i<n; i++){
-        }
+    while(ub - lb > 1){
+        int m = (ub + lb) / 2;
+        if(p(m)) ub = m;
+        else lb = m;
     }
-    b = 0;
-    for(i=lb; i < n; i++){
-        b = B[i] + b;
-    }
-    if(a < b) a = b;
-    printf("%d\n",a);
+    printf("%d\n",ub);
     return 0;
 }
